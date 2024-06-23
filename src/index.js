2f5f6caddac3e12decb2ca4666cd9d6c4f680908
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-06-23 21:29:52 +0530

    db connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config({ path: './.env' });
+
+import express from "express";
+import connectDB from "./db/index.js";
+
+const app = express();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`\nServer is running at port: ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
