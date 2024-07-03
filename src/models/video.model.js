ef464cac3276c9823b3da11b448066739a9a6da1
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-07-04 03:24:29 +0530

    bcrypt, jwt, aggrigate

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+  {
+    viedoFile:{
+        type:String, //cloudinary url
+required:true
+    },
+    thumbnail:{
+        type:String, //cloudinary url
+        required:true
+    },
+    title:{
+        type:String, 
+        required:true
+    },
+    description:{
+        type:String, 
+        required:true
+    },
+    duration:{
+        type:Number, 
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+  },
+  { timestamps: true }
+);
+
+mongoose.plugin(mongooseAggregatePaginate)
+// we can write aggregate queries now
+export const Video = mongoose.model.apply("Video", videoSchema);
