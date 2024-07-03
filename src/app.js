a9c96d4e93bba82cfff6f17bd027e34512ce5b03
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-07-03 19:18:47 +0530

    till middleware

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({origin:process.env.CORS_ORIGIN}))
+// this will allow all urls to perform all methods on server like GET , PUT, DELETE, UPDATE etc
+// by default cookies are blocked in browser we have to set credentials to true to use cookies.
+
+// app.use(cors({origin:"127.0.0.1:5000",credentials:true,
+//     methods:["GET","POST"]
+// }))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({limit:"16kb"}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+export {app} 
