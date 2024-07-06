475c995f19398713cd2da0972d8416fb56b7d2cc
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-07-07 02:01:39 +0530

    user basic controller and route setup

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({origin:process.env.CORS_ORIGIN}))
+app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}))
 // this will allow all urls to perform all methods on server like GET , PUT, DELETE, UPDATE etc
 // by default cookies are blocked in browser we have to set credentials to true to use cookies.
 
@@ -15,7 +15,12 @@ app.use(cors({origin:process.env.CORS_ORIGIN}))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({limit:"16kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
 
 export {app} 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import dotenv from "dotenv";
-dotenv.config({ path: './.env' });
-
-import express from "express"; 
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
 
-const app = express();
 
 connectDB()
-  .then(() => {
+.then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`\nServer is running at port: ${process.env.PORT || 8000}`);
-    });
-  })
-  .catch((err) => {
-    console.log("MONGO db connection failed !!!", err);
-  });
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
