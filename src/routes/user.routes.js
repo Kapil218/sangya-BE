28cfae23ae5772234532ec9d770b1832c1403fbf
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-08-10 01:36:01 +0530

    all routes added, watch histry and profile page using aggrigation, subscription model added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import {
-  loginUser,
-  logoutUser,
-  registerUser,
+import { 
+  loginUser, 
+  logoutUser, 
+  registerUser, 
+  refreshAccessToken, 
+  changeCurrentPassword, 
+  getCurrentUser, 
+  updateUserAvatar, 
+  updateUserCoverImage, 
+  getUserChannelProfile, 
+  getWatchHistory 
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -25,5 +32,20 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJwt, logoutUser);
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router;
