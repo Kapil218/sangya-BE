c36c961721f1e24c17800ce1b83c1b3e40d0b9af
kapil singh rathore <kapilsinghrathore218@gmail.com>
2024-08-18 03:56:10 +0530

    comment controllers added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}))
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 // this will allow all urls to perform all methods on server like GET , PUT, DELETE, UPDATE etc
 // by default cookies are blocked in browser we have to set credentials to true to use cookies.
 
@@ -12,17 +12,18 @@ app.use(cors({origin:process.env.CORS_ORIGIN,credentials:true}))
 //     methods:["GET","POST"]
 // }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes import
-import userRouter from "./routes/user.routes.js"
-import videoRoute from "./routes/video.routes.js"
-
+import userRouter from "./routes/user.routes.js";
+import videoRoute from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 // routes declaration
-app.use("/api/v1/users",userRouter)
-app.use("/api/v1/videos",videoRoute)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRoute);
+app.use("/api/v1/comments", commentRouter);
 
-export {app} 
+export { app };
---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,127 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
+import { response } from "express";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, itemsPerPage = 10 } = req.query;
+
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        "owner.username": 1,
+        "owner.avatar": 1,
+        "owner.fullName": 1,
+        totalComments: 1,
+      },
+    },
+    {
+      $skip: (page - 1) * itemsPerPage,
+    },
+    {
+      $limit: itemsPerPage,
+    },
+  ]);
+
+  if (!comments?.length) {
+    throw new ApiError(404, "no comments have found");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { totalComments: comments.length, data: comments },
+        "comments fetched successfully"
+      )
+    );
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (content.trim() == "") {
+    throw new ApiError(400, "comment field can not be empty");
+  }
+  if (videoId.trim() == "") {
+    throw new ApiError(400, "video id required");
+  }
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(401, "video can't be fetched");
+  }
+
+  const comment = await Comment.create({
+    owner: req.user,
+    content: content,
+    video: video,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "commented successfully"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  // TODO: update a comment
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  const comment = await Comment.findById(commentId);
+  if (comment.owner._id.toString() == req.user._id.toString()) {
+    var updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      {
+        $set: {
+          content: content,
+        },
+      },
+      { new: true }
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedComment, "comment updated successFully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  // TODO: delete a comment
+  const { commentId } = req.params;
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "comment not found");
+  }
+  if (comment.owner._id.toString() == req.user._id.toString()) {
+    await Comment.deleteOne({ _id: commentId });
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "comment deleteted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
